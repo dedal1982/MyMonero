6fa213efbfd8cc467a1c4374275976e19a214594
aldm <d44.a1@yandex.ru>
2024-09-28 17:08:15 +0300

    1

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const buttonCreate = document.querySelector(".button-create");
 const createPage = document.querySelector(".create-page");
+const cancelBtn = document.querySelector(".cancel-btn");
 
 if (buttonCreate) {
   buttonCreate.addEventListener("click", () => {
     createPage.classList.add("active");
+  });
+}
+
+if (cancelBtn) {
+  cancelBtn.addEventListener("click", () => {
+    createPage.classList.remove("active");
   });
 }
