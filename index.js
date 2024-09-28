2c8b23e0ded7cf95440586be488f3f3abb0f8890
aldm <d44.a1@yandex.ru>
2024-09-28 21:23:03 +0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const buttonCreate = document.querySelector(".button-create");
 const createPage = document.querySelector(".create-page");
 const cancelBtn = document.querySelector(".cancel-btn");
+const nextBtn = document.querySelector(".next-btn");
+const createGoBtn = document.querySelector(".create-page-go");
+const checkedGo = createGoBtn.querySelector(".checked-go");
+const createMnemonic = document.querySelector(".create-mnemonic");
 
 if (buttonCreate) {
   buttonCreate.addEventListener("click", () => {
@@ -11,5 +15,18 @@ if (buttonCreate) {
 if (cancelBtn) {
   cancelBtn.addEventListener("click", () => {
     createPage.classList.remove("active");
+  });
+}
+
+if (createGoBtn) {
+  createGoBtn.addEventListener("click", () => {
+    checkedGo.classList.toggle("active");
+    nextBtn.classList.toggle("active");
+  });
+}
+
+if (nextBtn) {
+  nextBtn.addEventListener("click", () => {
+    createMnemonic.classList.add("active");
   });
 }
